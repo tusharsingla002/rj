0b97f0f7432c5f3eaff40b278c65cadb0a45545f
tusharsingla002 <76396067+tusharsingla002@users.noreply.github.com>
2021-08-27 15:30:23 +0530

    Delete Notes directory

--- Notes/main.js
+++
@@ -1,6 +0,0 @@
-if (screen.width>750){
-    window.location.replace("http://tusharsingla002.github.io/project/index3.html")
-}
-if(screen.width<750){
-    window.location.replace("http://tusharsingla002.github.io/project/index2.html")
-}
