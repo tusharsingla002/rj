091ccc9e7326525e897a3df70d472f168fe427a7
tusharsingla002 <76396067+tusharsingla002@users.noreply.github.com>
2021-08-27 15:31:22 +0530

    Add files via upload

---
+++ notes/main.js
@@ -0,0 +1,6 @@
+if (screen.width>750){
+    window.location.replace("http://tusharsingla002.github.io/rj/notes/index3.html")
+}
+if(screen.width<750){
+    window.location.replace("http://tusharsingla002.github.io/rj/notes/index2.html")
+}
